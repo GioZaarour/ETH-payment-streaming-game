821539398eca1c855a3d8a90420eae957bcb01c2
Giovanni Zaarour <giozaarour@gmail.com>
2022-12-17 23:20:56 -0800

    Update HotPotato.sol

--- contracts/HotPotato.sol
+++ contracts/HotPotato.sol
@@ -1,11 +1,49 @@
-pragma solidity 0.8.9;
-import "@uma/core/contracts/oracle/interfaces/OptimisticOracleV2Interface.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { 
+    IAlluoToken 
+} from "alluo-protocole/contracts/interfaces/IAlluoToken.sol";
+
+import { 
+    ISuperfluid,
+    ISuperfluidToken
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import { 
+    IConstantFlowAgreementV1 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    CFAv1Library
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+
+import 'lib/protocol/packages/core/contracts/oracle/interfaces/OptimisticOracleV2Interface.sol';
+
+contract Vault {
+    
+}
 
 contract HotPotato {
-    // Create an Optimistic oracle instance at the deployed address on Görli.
-    OptimisticOracleV2Interface oo = OptimisticOracleV2Interface(0xA5B9d8a0B0Fa04Ba71BDD68069661ED5C0848884); //CHANGE TO MAINNET VERSION LATER
+    // **** GAME LOGIC GLOBAL VARS ****
 
-    address holder;
+    // 0: Game has not been initialized
+    // 1: Game is in lobby, users are joining
+    // 2: Game is active
+    uint8 gameState;
+    address HARD_RECIPIENT = 0xE091504578626BB2A83416A20E986b23FfEa95cb;
+    address stibAlluoAddress = 0x2efC02E2cDCC1EF699f4aF7E98b20f8A2a30923d;
+    
+    address[] players;
+    uint256 holder;
+
+    uint256 MAX_PLAYERS = 5;
+
+    // **** UMA PROTOCOL GLOBAL VARS ****
+
+    // Create an Optimistic oracle instance at the deployed address on Polygon Mumbai.
+    OptimisticOracleV2Interface oo = OptimisticOracleV2Interface(0x60E6140330F8FE31e785190F39C1B5e5e833c2a9); //CHANGE TO MAINNET VERSION LATER
+
     address target;
     bytes targetString;
 
@@ -14,16 +52,71 @@ contract HotPotato {
     bytes ancillaryData;
     uint256 requestTime = 0; // Store the request time so we can re-use it later.
 
-    //this constructor is temporary (for testing)
-    constructor(address _firstHolder) {
-        holder = _firstHolder;
+
+    // **** ALLUO STREAMING LOGIC GLOBAL VALS ****
+
+    Vault fundsVault;
+    using CFAv1Library for CFAv1Library.InitData;
+    
+    //initialize cfaV1 variable
+    CFAv1Library.InitData public cfaLib;
+    ISuperfluidToken public alluoToken = ISuperfluidToken(0xEB796bdb90fFA0f28255275e16936D25d3418603);
+
+    constructor(
+        ISuperfluid host
+    ) {
+    
+        //initialize InitData struct, and set equal to cfaV1
+        cfaLib = CFAv1Library.InitData(
+        host,
+            //here, we are deriving the address of the CFA using the host contract
+            IConstantFlowAgreementV1(
+                address(0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873)
+            )
+
+            //host.getAgreementClass(
+                //     keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
+                // )
+        );
+        
+        fundsVault = new Vault();
+    }
+
+    function readGameState() public view returns (uint8) {
+        return gameState;
+    }
+
+    function setGameState(uint8 _gameState) public {
+        gameState = _gameState;
+        
+        if (gameState == 1) {
+            initializeGame();
+        }
+    }
+
+    function joinGame() public {
+        require(players.length < MAX_PLAYERS);
+
+        // TRANSFER ALLUO tokens
+        players.push(msg.sender);
+
+    }
+
+    function initializeGame() internal {
+        holder = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % players.length;
+
+        startStream(address(this));
+    }
+
+    function streamtest() public {
+
     }
 
     // Submit a data request to the Optimistic oracle.
     //potato holder requests yes or no query "give hot potato to [target] player" then proposes yes
-    function tossPotato1(address _target, bytes memory _targetString) public {
+    function tossPotatoInitiate(address _target, bytes memory _targetString) public {
         //[require there is no ongoing toss]?
-        require(msg.sender == holder, "Cannot toss potato without being holder");
+        require(msg.sender == players[holder], "Cannot toss potato without being holder");
 
         // Post the question in ancillary data
         ancillaryData = bytes.concat("Q:Give hot potato to ", _targetString , " ? A:1 for yes. 0 for no.");
@@ -41,8 +134,8 @@ contract HotPotato {
         oo.setCustomLiveness(identifier, requestTime, ancillaryData, 120);
     }
 
-    function tossPotato2() public {
-        require(msg.sender == holder, "Cannot toss potato without being holder");
+    function tossPotato() public {
+        require(msg.sender == players[holder], "Cannot toss potato without being holder");
 
         //now propose yes
         oo.proposePriceFor(msg.sender,address(this), identifier, requestTime, ancillaryData, 1000000000000000000); //1e18 = yes
@@ -64,12 +157,48 @@ contract HotPotato {
 
         //[include logic here to send the target the hot potato] (in other words send the superfluid stream)
         //set new holder equal to target
-        holder = target;
+        uint256 target_id;
+
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i] == target) {
+                holder = i;
+                target_id = i;
+            }
+        }
+        transferPotato(target_id);
     }
 
     // Fetch the resolved price from the Optimistic Oracle that was settled.
     function getSettledData() public view returns (int256) {
         return oo.getRequest(address(this), identifier, requestTime, ancillaryData).resolvedPrice;
+    }
+
+    function transferPotato(uint256 newHolder) public {
+        transferStream(newHolder);
+    }
+
+    // *** SUPERFLUID LOGIC ***
+
+
+    function startStream(address recipient) public {
+        // initialize superfluid stream, send to hardcode
+        // bytes memory context = cfaLib.authorizeFlowOperatorWithFullControl(address(this), alluoToken);
+        cfaLib.createFlow(address(fundsVault), alluoToken, 1);
+    }
+
+    function transferStream(uint256 newHolder) public {
+        cfaLib.deleteFlowByOperator(players[holder], players[newHolder], alluoToken);
+        
+        // Split funds sent to the vault 
+        for (uint256 i = 0; i < players.length; i++) {
+            // Don't send funds to the holder (obviously)
+            if (i != holder) {
+                payable(players[i]).transfer(address(fundsVault).balance / (players.length -1));
+            }
+        }
+
+        cfaLib.createFlow(address(fundsVault), alluoToken, 1);
+
     }
 
 }
